96b12ebd5d7f6e7c8e47879a4147a27219d56522
Khilaja <1hvostaja@gmail.com>
2022-07-14 18:31:15 +0300

    hw updates

--- JavaScript/js_hw_6/js/script.js
+++ JavaScript/js_hw_6/js/script.js
@@ -1,5 +1,29 @@
-let student = {
-    name: "Your name",
-    age: "Your age",
-    id: 1,
-}
+// let student = {
+//     name: "Tanya",
+//     age: 31,
+//     id: 1,
+// }
+
+// let studentArr = Object.entries(student);
+
+// let studentMap = new Map(studentArr);
+
+// studentMap.set("course", "Frontend");
+// studentMap.set("isSuccesfull", false);
+
+// console.log(studentMap.get("name"));
+
+// function showMapInfo (collection) {
+// for (let keys of collection.keys()) {
+//     console.log(keys);
+// }
+// for (let values of collection.values()) {
+//     console.log(values);
+// }
+// for (let entries of collection.entries()) {
+//     console.log(entries);
+// }
+// }
+// showMapInfo(studentMap);
+
+
